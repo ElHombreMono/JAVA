d278829e34830b62c7100efff01dd2afc413f8c3
ElHombreMono <97600396+ElHombreMono@users.noreply.github.com>
2022-04-19 10:08:23 +0200

    Update Persona.java

--- Persona/Persona.java
+++ Persona/Persona.java
@@ -10,4 +10,9 @@ public class Persona{
 		this.nombre = "Pablo";
 		this.dni = "12345678A";
 	}
+
+	// --------------------------------------------------------toString:
+	public String toString(){
+		return "Me llamo " + this.nombre + " y mi dni es " + this.dni + ".";
+	}
 }
