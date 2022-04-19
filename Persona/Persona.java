c4888e8cef9dc42a1f7f6e79b91a939e40b9a6cd
ElHombreMono <97600396+ElHombreMono@users.noreply.github.com>
2022-04-19 10:35:44 +0200

    Update Persona.java

--- Persona/Persona.java
+++ Persona/Persona.java
@@ -10,6 +10,23 @@ public class Persona{
 		this.nombre = "Pablo";
 		this.dni = "12345678A";
 	}
+	public Persona(String nombre, String dni){
+		this.nombre = nombre;
+		this.dni = dni;
+	}
+	// -------------------------------------------------------- GET/SET:
+	public String getNombre(){
+		return this.nombre;
+	}
+	public String getDni(){
+		return this.dni;
+	}
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	public void setDni(String dni){
+		this.dni = dni;
+	}
 
 	// --------------------------------------------------------toString:
 	public String toString(){
